686152bc35b8e95f20fac53e78435b6b6e522737
Vlad Fedorovich <72762763+sskef-web@users.noreply.github.com>
2024-10-29 15:03:49 +0300

    Update QR-code scanner

--- lib/pages/QRViewExample.dart
+++ lib/pages/QRViewExample.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:achieve_club_mobile_admin/main.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+import '../main.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -16,19 +17,18 @@ class QRViewExample extends StatefulWidget {
 class _QRViewExampleState extends State<QRViewExample> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
-  QRViewController? controller;
+  MobileScannerController controller = MobileScannerController();
 
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
   @override
   void reassemble() {
     super.reassemble();
     if (Platform.isAndroid) {
-      controller!.pauseCamera();
+      controller.stop();
     } else if (Platform.isIOS) {
-      controller!.resumeCamera();
+      controller.start();
     }
   }
+
   Future<String?> loadCookies() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString('cookies');
@@ -40,7 +40,7 @@ class _QRViewExampleState extends State<QRViewExample> {
   }
 
   Future<void> refreshToken() async {
-    var refreshUrl = Uri.parse('${baseURL}auth/refresh');
+    var refreshUrl = Uri.parse('${baseURL}api/auth/refresh');
     var cookies = await loadCookies();
 
     var response = await http.get(refreshUrl, headers: {
@@ -59,20 +59,21 @@ class _QRViewExampleState extends State<QRViewExample> {
   }
 
   Future<void> onAchieveComplete() async {
-    String data = result!.code!;
+    String data = result!.rawValue!;
     var dataParts = data.split(':');
     debugPrint("${dataParts[0]}\n${dataParts[1]}");
-    var url = Uri.parse('http://142.93.231.78:8080/api/completedachievements');
+    var url = Uri.parse('${baseURL}api/completedachievements');
     SharedPreferences prefs = await SharedPreferences.getInstance();
     var cookies = prefs.getString('cookies');
 
     var headers = {'Content-Type': 'application/json', 'Cookie': cookies!};
     var achievementIds =
-        dataParts.sublist(1);
+    dataParts.sublist(1);
     var body = jsonEncode({
       "userId": dataParts[0],
       "achievementIds": achievementIds,
     });
+    debugPrint('RESPONSE BODY - ${body}');
     var response = await http.post(url, body: body, headers: headers);
     if (response.statusCode == 200) {
       showDialog(
@@ -107,6 +108,39 @@ class _QRViewExampleState extends State<QRViewExample> {
       await refreshToken();
       await onAchieveComplete();
     }
+    else {
+      showDialog(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Достижение не выполнено.\nОшибка: ${response.body}\nКод статуса - ${response.statusCode}',
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 8.0,),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(dialogContext).pop();
+                    },
+                    child: const Text('Закрыть'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
     debugPrint("${response.statusCode} (${response.body})");
     setState(() {
       result = null;
@@ -120,37 +154,48 @@ class _QRViewExampleState extends State<QRViewExample> {
         children: <Widget>[
           Expanded(
             flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
+            child: MobileScanner(
+              controller: controller,
+              onDetect: (BarcodeCapture barcode) {
+                setState(() {
+                  result = barcode.barcodes.first;
+                });
+              },
             ),
           ),
           Expanded(
-            flex: 1,
+            flex: 2,
             child: Center(
               child: (result == null)
                   ? const Text("Отсканируйте!")
-                  : ElevatedButton(
-                      onPressed: onAchieveComplete, child: const Text('Выполнить')),
+                  : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: onAchieveComplete,
+                    child: const Text('Выполнить'),
+                  ),
+                  result != null ? const SizedBox(height: 8.0) : Container(),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        result = null;
+                      });
+                    },
+                    child: const Text('Сбросить сканированный QR-код'),
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
   }
 
-  void _onQRViewCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
-
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 }
